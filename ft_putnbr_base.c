43ba798fe24ef576486f82d0c9bf5dc95e8be939
Hugo Delmas <hdelmas@fu-r1-p7.s19.be>
2022-10-24 07:52:16 +0200

    yes

--- ft_putnbr_base.c
+++ ft_putnbr_base.c
@@ -6,45 +6,19 @@
 /*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/21 17:57:40 by hdelmas           #+#    #+#             */
-/*   Updated: 2022/10/21 17:59:43 by hdelmas          ###   ########.fr       */
+/*   Updated: 2022/10/24 07:51:20 by hdelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_condititon(long int base_len, char *base)
-{	
-	long int	i;
-	long int	j;
-
-	if (base_len <= 1)
-		return (0);
-	i = 0;
-	while (base[i] != '\0')
-	{
-		j = i + 1;
-		if (base[i] == 45 || base[i] == 43 || base[i] < 33)
-			return (0);
-		while (base[j] != '\0')
-		{
-			if (base[i] == base[j])
-				return (0);
-			j++;
-		}
-		i++;
-	}
-	return (1);
-}
-
 int	ft_putnbr_base_fd(unsigned long int nbr, char *base, int fd)
 {
-	size_t	base_len;
-	int		nbr_len;
+	unsigned long int	base_len;
+	int					nbr_len;
 
 	nbr_len = 0;
 	base_len = ft_strlen(base);
-	if (ft_condititon(base_len, base) == 0)
-		return (-1);
 	if (nbr < base_len)
 		nbr_len += ft_putchar_fd(base[nbr], fd);
 	else
